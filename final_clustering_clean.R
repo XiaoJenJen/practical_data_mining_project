4d248ac292b26b51c242c07df1415bbebc2d6f0f
Ti-Tai Wang <titai.wang@gmail.com>
2018-12-05 18:01:38 -0700

    add_classification

--- final_clustering_clean.R
+++ final_clustering_clean.R
@@ -247,6 +247,336 @@ b$nb_day = as.numeric(factor(accidents$Day_of_Week, levels = c("Monday", "Tuesda
 summary(b)
 
 ####################
+# Classification   #
+####################
+
+acc_cleaned <- read.csv(file = "Accident_data_cleaned.csv",
+                        header = TRUE,
+                        sep = ",",
+                        stringsAsFactors = FALSE)
+
+acc_rural <- read.csv(file = "Accident_data_rural_cleaned.csv",
+                      header = TRUE,
+                      sep = ",",
+                      stringsAsFactors = FALSE)
+
+acc_cleaned = acc_cleaned[, -c(1, 21)]
+acc_rural = acc_rural[, -c(1, 21)]
+
+# copy format from Moonsung's code
+accidents = read.csv(file = "./Accident_Casualty_2016.csv",
+                     header = TRUE,
+                     sep = ",",
+                     stringsAsFactors = FALSE)
+
+accidents = accidents[, -c(7, 18)]
+
+#apply preprocessing code again
+
+acc_rural = acc_rural[, colnames(accidents)]
+acc_cleaned = acc_cleaned[, colnames(accidents)]
+
+acc = accidents
+accidents = acc_rural
+
+a = accidents
+
+acc_numeric = a[1]
+acc_factor = a[1]
+acc_onehot = accidents[1]
+acc_binary = accidents[1]
+
+#transform Date into Accident severity
+unique(accidents$Accident_Severity)
+acc_factor$Accident_Severity = factor(accidents$Accident_Severity)
+acc_numeric$Accident_Severity = as.numeric(factor(accidents$Accident_Severity, levels = c("Slight", "Serious", "Fatal")))
+for(unique_value in unique(accidents$Accident_Severity)){
+  acc_onehot[paste("Accident_Severity", unique_value, sep = ".")] <- ifelse(accidents$Accident_Severity == unique_value, 1, 0)
+}
+unique(acc_onehot$Accident_Severity.Fatal)
+acc_onehot[2,]
+
+acc_binary$Accident_Severity <- matrix(
+  as.integer(intToBits(as.integer(as.factor(accidents$Accident_Severity)))),
+  ncol = 32,
+  nrow = length(accidents$Accident_Severity),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(accidents$Accident_Severity)) + 1)/log(2))]
+acc_binary[1,]
+
+#transform Date into 4 seasons, we're using just single year data, sesonal value is not important..?
+unique(accidents$Date)
+a$Date = sapply(strsplit(accidents$Date,"-"),
+                function(x) {
+                  x <- as.numeric(x)
+                  x[2]
+                }
+)
+acc_factor$Month = factor(a$Date, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), labels = c(1,2,3,4,5,6,7,8,9,10,11,12))
+acc_numeric$Month = as.numeric(factor(a$Date, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)))
+
+#season
+a$Season = factor(a$Date, levels = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
+                  labels = c("Winter", "Winter", "Spring", "Spring", "Spring", "Summer", "Summer", "Summer", "Autumn", "Autumn", "Autumn", "Winter"))
+unique(a$Season)
+acc_factor$Season = factor(a$Season, levels = c("Winter", "Spring", "Summer", "Autumn"))
+acc_numeric$Season = as.numeric(factor(a$Season, levels = c("Winter", "Spring", "Summer", "Autumn")))
+for(unique_value in unique(a$Season)){
+  acc_onehot[paste("Season", unique_value, sep = ".")] <- ifelse(a$Season == unique_value, 1, 0)
+}
+acc_binary$Season <- matrix(
+  as.integer(intToBits(as.integer(as.factor(a$Season)))),
+  ncol = 32,
+  nrow = length(a$Season),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(a$Season)) + 1)/log(2))]
+
+
+#day
+unique(accidents$Day_of_Week)
+acc_factor$Day_of_Week = factor(accidents$Day_of_Week)
+acc_numeric$Day_of_Week = as.numeric(factor(accidents$Day_of_Week, levels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")))
+for(unique_value in unique(accidents$Day_of_Week)){
+  acc_onehot[paste("Day_of_Week", unique_value, sep = ".")] <- ifelse(accidents$Day_of_Week == unique_value, 1, 0)
+}
+acc_binary$Day_of_Week <- matrix(
+  as.integer(intToBits(as.integer(as.factor(accidents$Day_of_Week)))),
+  ncol = 32,
+  nrow = length(accidents$Day_of_Week),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(accidents$Day_of_Week)) + 1)/log(2))]
+
+
+#Light_Condition
+acc_factor$Light_Conditions = factor(accidents$Light_Conditions)
+for(unique_value in unique(accidents$Light_Conditions)){
+  acc_onehot[paste("Light_Conditions", unique_value, sep = ".")] <- ifelse(accidents$Light_Conditions == unique_value, 1, 0)
+}
+acc_binary$Light_Conditions <- matrix(
+  as.integer(intToBits(as.integer(as.factor(accidents$Light_Conditions)))),
+  ncol = 32,
+  nrow = length(accidents$Light_Conditions),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(accidents$Light_Conditions)) + 1)/log(2))]
+
+
+#Road_Surface_Conditions
+acc_factor$Road_Surface_Conditions = factor(accidents$Road_Surface_Conditions)
+for(unique_value in unique(accidents$Road_Surface_Conditions)){
+  acc_onehot[paste("Road_Surface_Conditions", unique_value, sep = ".")] <- ifelse(accidents$Road_Surface_Conditions == unique_value, 1, 0)
+}
+acc_binary$Road_Surface_Conditions <- matrix(
+  as.integer(intToBits(as.integer(as.factor(accidents$Road_Surface_Conditions)))),
+  ncol = 32,
+  nrow = length(accidents$Road_Surface_Conditions),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(accidents$Road_Surface_Conditions)) + 1)/log(2))]
+
+
+#Road_Type
+acc_factor$Road_Type = factor(accidents$Road_Type)
+for(unique_value in unique(accidents$Road_Type)){
+  acc_onehot[paste("Road_Type", unique_value, sep = ".")] <- ifelse(accidents$Road_Type == unique_value, 1, 0)
+}
+acc_binary$Road_Type <- matrix(
+  as.integer(intToBits(as.integer(as.factor(accidents$Road_Type)))),
+  ncol = 32,
+  nrow = length(accidents$Road_Type),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(accidents$Road_Type)) + 1)/log(2))]
+
+#Time
+unique(accidents$Time)
+a$Time = sapply(strsplit(accidents$Time,":"),
+                function(x) {
+                  x <- as.numeric(x)
+                  x[1]+x[2]/60
+                }
+)
+# Midnight - 6am = Night; 6am - Noon = Morning; Noon - 6pm = afternoon; 6pm - Midnight = evening
+a$Time = cut(a$Time, breaks=4, labels=c("Night", "Morning", "Afternoon", "Evening"))
+acc_factor$Time = factor(a$Time)
+acc_numeric$Time = as.numeric(factor(a$Time, levels = c("Night", "Morning", "Afternoon", "Evening")))
+for(unique_value in unique(a$Time)){
+  acc_onehot[paste("Time", unique_value, sep = ".")] <- ifelse(a$Time == unique_value, 1, 0)
+}
+acc_binary$Time <- matrix(
+  as.integer(intToBits(as.integer(as.factor(a$Time)))),
+  ncol = 32,
+  nrow = length(a$Time),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(a$Time)) + 1)/log(2))]
+
+#Weather_Conditions
+acc_factor$Weather_Conditions = factor(accidents$Weather_Conditions)
+for(unique_value in unique(accidents$Weather_Conditions)){
+  acc_onehot[paste("Weather_Conditions", unique_value, sep = ".")] <- ifelse(accidents$Weather_Conditions == unique_value, 1, 0)
+}
+acc_binary$Weather_Conditions <- matrix(
+  as.integer(intToBits(as.integer(as.factor(accidents$Weather_Conditions)))),
+  ncol = 32,
+  nrow = length(accidents$Weather_Conditions),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(accidents$Weather_Conditions)) + 1)/log(2))]
+
+
+#Age_Band_of_Driver
+acc_factor$Age_Band_of_Driver = factor(accidents$Age_Band_of_Driver)
+acc_numeric$Age_Band_of_Driver = as.numeric(factor(accidents$Age_Band_of_Driver, levels = c("Data missing or out of range", "6 - 10", "11 - 15", "16 - 20", "21 - 25", "26 - 35", "36 - 45", "56 - 65", "46 - 55", "66 - 75", "Over 75")))
+for(unique_value in unique(accidents$Age_Band_of_Driver)){
+  acc_onehot[paste("Age_Band_of_Driver", unique_value, sep = ".")] <- ifelse(accidents$Age_Band_of_Driver == unique_value, 1, 0)
+}
+acc_binary$Age_Band_of_Driver <- matrix(
+  as.integer(intToBits(as.integer(as.factor(accidents$Age_Band_of_Driver)))),
+  ncol = 32,
+  nrow = length(accidents$Age_Band_of_Driver),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(accidents$Age_Band_of_Driver)) + 1)/log(2))]
+
+
+#Sex_of_Driver
+acc_factor$Sex_of_Driver = factor(accidents$Sex_of_Driver)
+for(unique_value in unique(accidents$Sex_of_Driver)){
+  acc_onehot[paste("Sex_of_Driver", unique_value, sep = ".")] <- ifelse(accidents$Sex_of_Driver == unique_value, 1, 0)
+}
+acc_binary$Sex_of_Driver <- matrix(
+  as.integer(intToBits(as.integer(as.factor(accidents$Sex_of_Driver)))),
+  ncol = 32,
+  nrow = length(accidents$Sex_of_Driver),
+  byrow = TRUE
+)[, 1:ceiling(log(length(unique(accidents$Sex_of_Driver)) + 1)/log(2))]
+
+
+#SVM
+#Attach Packages
+install.packages("tidyverse")
+install.packages("kernlab")
+install.packages("e1071")
+install.packages("ISLR")
+install.packages("RColorBrewer")
+
+library(tidyverse)    # data manipulation and visualization
+library(kernlab)      # SVM methodology
+library(e1071)        # SVM methodology
+library(ISLR)         # contains example data set "Khan"
+library(RColorBrewer) # customized coloring of plot
+
+library("e1071")
+
+y1 <- acc_factor$Age_Band_of_Driver
+y2 <- acc_factor$Sex_of_Driver
+
+colnames(acc_onehot)
+colnames(acc_binary)
+#AgeBand
+x1 <- subset(acc_onehot, select = c(2:40,49,50))
+#Sex
+x2 <- subset(acc_onehot, select = c(2:48))
+
+n_row <- sample(x = rep(1:nrow(acc_factor)), size = round(nrow(acc_factor)/4), 
+                replace = FALSE)
+
+test_x1 <- x1[n_row, ]
+train_x1 <- x1[-n_row, ]
+
+test_y1 <- y1[n_row]
+train_y1 <- y1[-n_row]
+
+test_x2 <- x2[n_row, ]
+train_x2 <- x2[-n_row, ]
+
+test_y2 <- y2[n_row]
+train_y2 <- y2[-n_row]
+
+#predict AgeBand
+##Linear-err: 80.2%
+svm_model1 <- svm(train_y1~., dd, kernel = "linear", type = "C-classification")
+summary(svm_model1)
+pred1 <- predict(svm_model1, data.frame(test_x1))
+(t1 <- table(pred1, test_y1))
+(1-sum(diag(t1))/sum(t1))
+
+##Radial and 5fold-CV-err: 82.9%
+dd <- data.frame(train_x1, train_y1)
+
+res <- performanceEstimation(
+  PredTask(train_y1 ~ .,dd),
+  #run svm with different combinations of costs and gammas
+  workflowVariants(learner="svm",
+                   learner.pars=list(kernel = c("radial"),cost=c(1,10, 0.1, 0.5),gamma=c(0.1,1,2,0.5))), 
+  #run nnet with different combinations of hidden layer sizes and decays
+  EstimationTask(metrics="err", method=CV(nReps=1, nFolds=5)))
+
+summary(res)
+topPerformers(res)
+getWorkflow("svm.v4", res)
+
+svm_model_age <- svm(train_y1 ~., dd, kernel = "radial", gamma = 0.1, cost = 0.5)
+pred <- predict(svm_model_age, data.frame(test_x1))
+(t2 <- table(pred, test_y1))
+(1-sum(diag(t2))/sum(t2))
+
+#ANN-err: 83.3%
+# s = 2, d = 0.0001
+tmodel = tune.nnet(train_y1 ~ ., data = dd, size = 1:10, decay = c(0.001, 0.0001))
+summary(tmodel)
+
+(n <- nnet(train_y1 ~.,dd, size = 2, 
+           rang = 0.1, decay = 0.0001,
+           trace=FALSE, maxit=500))
+(pred <- predict(n, data.frame(test_x1), type = "class"))
+(t3 <- table(pred, test_y1))
+(1-sum(diag(t3))/sum(t3))
+plotnet(n)
+
+#predict sex err: 42.8%
+svm_model2 <- svm(train_x2, train_y2, kernel = "linear")
+summary(svm_model2)
+pred2 <- predict(svm_model2, test_x2)
+(t4 <- table(pred2, test_y2))
+(1-sum(diag(t4))/sum(t4))
+
+##Radial err:38.6%
+dd2 <- data.frame(train_x2, train_y2)
+
+res2 <- performanceEstimation(
+  PredTask(train_y2 ~ .,dd2),
+  #run svm with different combinations of costs and gammas
+  workflowVariants(learner="svm",
+                   learner.pars=list(kernel = c("radial"),cost=c(1,10, 0.1,0.5),gamma=c(0.1,1,2,0.5))), 
+  #run nnet with different combinations of hidden layer sizes and decays
+  EstimationTask(metrics="err", method=CV(nReps=1, nFolds=5)))
+
+summary(res2)
+topPerformers(res2)
+getWorkflow("svm.v5", res2)
+
+svm_model_se2 <- svm(train_x2, train_y2, kernel = "radial", gamma = 1, cost = 1)
+pred <- predict(svm_model_se2, test_x2)
+(t5 <- table(pred, test_y2))
+(1-sum(diag(t5))/sum(t5))
+
+#ANN-err: 39.1%
+(n <- nnet(train_y2 ~.,dd2, size = 2, 
+           rang = 0.1, decay = 0.0001,
+           trace=FALSE, maxit=500))
+(pred <- predict(n, data.frame(test_x2), type = "class"))
+(t6 <- table(pred, test_y2))
+(1-sum(diag(t6))/sum(t6))
+
+# s = 2, d = 0.0001
+tmodel3 = tune.nnet(train_y2 ~ ., data = dd2, size = 1:10, decay = c(0.001, 0.0001))
+summary(tmodel3)
+
+
+# Precision and Recall (put tX into t)
+(precision = sum(diag(t))/409)
+(recall = sum(t[1,])/sum(t[,1]))
+(f1 = 2 * precision * recall / (precision + recall))
+
+
+
+####################
 # MCA              #
 ####################
 c <- acc_factor
